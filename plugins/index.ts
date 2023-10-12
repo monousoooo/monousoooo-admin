b28d361d39c5f58b35131da95e6343bf04148a15
张浩杰 <havoc_zhang@yeah.net>
2023-10-12 21:24:17 +0800

    新增unplugin-auto-import依赖

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,6 +1,19 @@
 import type { PluginOption } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export function createVitePlugins() {
-  const vitePluginList: (PluginOption | PluginOption[])[] = []
+  const vitePluginList: (PluginOption | PluginOption[])[] = [
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/core',
+        'pinia',
+      ],
+      dts: 'types/auto-imports.d.ts',
+      dirs: ['src/stores', 'src/composables'],
+    }),
+  ]
   return vitePluginList
 }
